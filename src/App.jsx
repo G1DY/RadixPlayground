50ddd46a5c42b4cf653e575d655c0605026afc50
gideon <kipkerring.gedion@gmail.com>
2025-02-14 11:33:46 +0300

    using tailwindcss4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline bg-amber-50">Hello World!</h1>
-      <p className="text-2xl">Testing</p>
+      <p className="text-3xl">Testing</p>
     </div>
   );
 };
